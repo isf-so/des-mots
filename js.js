010986a5cbe25166d8b14f971f05b80f547175f0
s <glcsofiane40@gmail.com>
2024-03-08 13:52:57 +0100

    1

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
 function checkPassword() {
   var password = document.getElementById('password').value;
+  
   if (password === 'bonheur') {
     window.location.href = 'pages/accueil.html'; // Rediriger vers la page suivante après la connexion réussie
   } else if (password === 'heureux') {
     window.location.href = 'pages2/2page.html'; // Rediriger vers la page suivante après la connexion réussie
+  } else if (password === 'help') {
+    window.location.href = 'help/help.html'; // Rediriger vers la page suivante après la connexion réussie
   } else {
     alert('Mot de passe incorrect. Veuillez réessayer.');
   }
